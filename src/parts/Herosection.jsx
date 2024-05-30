288f7bd5e192687493437167ccba4c14c5d897a2
SYED SAAD ALI <76819782+syed-saad-ali-046@users.noreply.github.com>
2024-05-30 15:33:09 +0500

    check

--- src/parts/Herosection.jsx
+++ src/parts/Herosection.jsx
@@ -27,7 +27,7 @@ const HeroSlider = ({ slides }) => {
 
   return (
     <div
-      className="relative md:mr-20 main h-[500px] overflow-hidden mt-5"
+      className="slider-container"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       style={{ cursor: 'pointer' }}
@@ -35,41 +35,21 @@ const HeroSlider = ({ slides }) => {
       {slides.map((slide, index) => (
         <div
           key={index}
-          className={`absolute inset-0  flex items-center justify-center transition-opacity duration-100 ease-in-out ${
-            index === currentSlide ? 'opacity-100' : 'opacity-0'
-          }`}
+          className={`slide ${index === currentSlide ? 'active' : ''}`}
           style={{
             backgroundImage: `url(${slide.imageUrl})`,
-            backgroundSize: 'contain',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
           }}
         >
-          <div className="absolute inset-0 bg-black" style={{ opacity: isHovered ? 0.5 : 0 }}></div>
-          <div
-            className={`container mx-auto px-4 text-center z-20 transition-opacity duration-300 ${
-              isHovered ? 'opacity-100' : 'opacity-0'
-            }`}
-          >
-            <h1 className="text-4xl font-bold text-white md:text-5xl lg:text-6xl">
-              {slide.heading}
-            </h1>
-            <p className="text-xl text-gray-200 mt-4 md:text-2xl lg:text-3xl">
-              {slide.description}
-            </p>
-            {slide.cta && (
-              <p className="text-xl text-gray-200 mt-4 md:text-1xl lg:text-1xl underline">
-                {slide.cta}
-              </p>
-            )}
+          <div className="overlay" style={{ opacity: isHovered ? 0.5 : 0 }}></div>
+          <div className={`content ${isHovered ? 'show' : ''}`}>
+            <h1>{slide.heading}</h1>
+            <p>{slide.description}</p>
+            {slide.cta && <p className="cta">{slide.cta}</p>}
           </div>
         </div>
       ))}
-      <button
-        className="absolute top-1/2 left-0 ml-1 transform -translate-y-1/2 text-white bg-black bg-opacity-35 p-2 rounded-full"
-        onClick={handlePrevSlide}
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-10 h-10">
+      <button className="prev-button" onClick={handlePrevSlide}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="icon">
           <path
             fillRule="evenodd"
             d="M10.72 11.47a.75.75 0 0 0 0 1.06l7.5 7.5a.75.75 0 1 0 1.06-1.06L12.31 12l6.97-6.97a.75.75 0 0 0-1.06-1.06l-7.5 7.5Z"
@@ -82,11 +62,8 @@ const HeroSlider = ({ slides }) => {
           />
         </svg>
       </button>
-      <button
-        className="absolute top-1/2 button lg:right-12 md:right-10 transform -translate-y-1/2 text-white bg-black bg-opacity-35 p-2 rounded-full"
-        onClick={handleNextSlide}
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-10 h-10">
+      <button className="next-button" onClick={handleNextSlide}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="icon">
           <path
             fillRule="evenodd"
             d="M13.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L11.69 12l-6.97-6.97a.75.75 0 0 1 1.06-1.06l7.5 7.5Z"
