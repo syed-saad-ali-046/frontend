08e2658450cff26c1506bfed105afc1b2b12cb59
SYED SAAD ALI <76819782+syed-saad-ali-046@users.noreply.github.com>
2024-06-24 20:20:24 +0500

    Update Slider.jsx

--- src/Data/Slider.jsx
+++ src/Data/Slider.jsx
@@ -1,35 +1,53 @@
 const productdata = [
   {
     id:1,
-    name:"Nike Shoes",
-    price: 4000,
-    image:"https://www.hushpuppies.com.pk/cdn/shop/files/IMG_6933_0b303997-9d81-4022-a254-0950a5fa9231_360x.jpg?v=1709530824",
-    sizes:['7','8','9','10'],
-    colors:["blue", "green", "red"],
+    images:[
+      "https://noblecorner.com/wp-content/uploads/2023/10/Ahmad_Accord_09-450x450.jpg",
+      "https://www.dynastyfabrics.com/cdn/shop/files/black_ac825490-5964-4e0c-b9ad-afc256247295_720x.jpg?v=1713352010",
+      "https://www.dynastyfabrics.com/cdn/shop/files/maroon_720x.jpg?v=1713352011"
+    ],
+    name:"Ahmad Accord",
+    price: "4200",
+    colors: ["red", "blue", "green"],
+    sizes: ["S", "M", "L"], // Example sizes
   },
   {
     id:2,
-    name:"HAL SWIFT",
-    price:4999,
-    image:"https://www.hushpuppies.com.pk/cdn/shop/files/IMG_7661_360x.jpg?v=1711137138",
-    sizes:['6','8','9','10'],
-    colors:["blue", "violet", "red"],
+    images:[
+      "https://noblecorner.com/wp-content/uploads/2023/10/Chawla_Charli_05-595x595.jpg",
+      "https://www.dynastyfabrics.com/cdn/shop/files/darkgrey_18f2c9f7-3b9e-460f-a0bc-106c1c5f241a_720x.jpg?v=1716201619",
+      "https://www.dynastyfabrics.com/cdn/shop/files/sage_720x.jpg?v=1716201613"
+
+    ],
+    name: "Ahmad Hil Ton",
+    price: "4521",
+    colors: ["black", "white", "gray"], 
+    sizes: ["XS", "S", "M", "L", "XL"], 
   },
   {
     id:3,
-    name:"Dazzy Kinder",
-    price: 2200,
-    image:"https://www.hushpuppies.com.pk/cdn/shop/files/IMG_7677_360x.jpg?v=1711137341",
-    sizes:['6','8','9'],
-    colors:["blue", "black"],
+    images:[
+      "https://www.dynastyfabrics.com/cdn/shop/files/lightgrey_720x.jpg?v=1713330127",
+      "https://www.dynastyfabrics.com/cdn/shop/files/offwhite_f1498844-b9e6-41d0-a38e-cfede33b2537_720x.jpg?v=1713330126",
+      "https://www.dynastyfabrics.com/cdn/shop/files/Dynasty-Nuetral_c4d10c30-7d4c-49a3-8857-0c7297f8006c_720x.png?v=1713330397",
+    ],
+    name: "Chawla Charli",
+    price: "2800",
+    colors: ["purple", "pink", "violet"],
+    sizes: ["S", "M","L"], 
   },
   {
     id:4,
-    name:"Dawn Kinder",
-    price: 3599,
-    image:"https://www.hushpuppies.com.pk/cdn/shop/files/IMG_7528_f93fff98-0ad4-4ce5-a962-ce3cb49cc336_360x.jpg?v=1710833482",
-    sizes:['6','9','10'],
-    colors:["blue", "yellow", "red"],
+    images:[
+      "https://www.dynastyfabrics.com/cdn/shop/files/auqa_720x.jpg?v=1714372839",
+      "https://www.dynastyfabrics.com/cdn/shop/files/white_18e87d3d-08eb-4bbf-b6f6-55022251aea4_720x.jpg?v=1714453480",
+      "https://www.dynastyfabrics.com/cdn/shop/files/cream_495ce5fb-fa78-45a6-ad94-dfce9e7791c1_720x.jpg?v=1714453480",
+
+    ],
+    name: "Oregon",
+    price: "5500",
+    colors: ["Offwhite", "Aqua", "Sand"], // Example colors
+    sizes: ["S", "M","L"], // Example sizes
   },
 
 
