2f42a48bd28620ea4288255258bacfc304b8cf21
SYED SAAD ALI <76819782+syed-saad-ali-046@users.noreply.github.com>
2024-06-27 16:22:26 +0500

    Update Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import SignupForm from './components/Signupform';
 import SigninForm from './components/Signin';
@@ -12,8 +11,11 @@ import Toggle from './parts/Tooglecart';
 import Shipping from './components/Shipping';
 import AddressInfo from './components/Address';
 import Payment from './components/Payment';
-import AdminOrderList from './Admin/Order';
+import Policy from './components/Policy';
+import Product from './components/Products';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
 
 function Ways() {
     return (
@@ -30,8 +32,9 @@ function Ways() {
                     <Route path="tcart" element={<Toggle/>}/>
                     <Route path="/Shipping-Info" element={<Shipping/>}/>
                     <Route path="/Address" element={<AddressInfo/>}/>
-                    <Route path="/getorder" element={<AdminOrderList/>}/>
                     <Route path="/order-confirmation" element={<Payment/>}/>
+                    <Route path="/policy" element={<Policy/>}/>
+                    <Route path="/products" element={<Product/>}/>
 
                 </Routes>
         </Router>
